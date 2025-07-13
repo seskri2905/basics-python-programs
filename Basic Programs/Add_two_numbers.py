77e29411d7950c57b485834e450c9fb949a0860e
Sesha Krishnan <mseshu29@gmail.com>
2025-07-13 15:30:27 +0530

    Addition of python files in Basic Programs

--- Basic Programs/Add_two_numbers.py
+++ Basic Programs/Add_two_numbers.py
@@ -7,11 +7,29 @@ add = a + b
 print(add) """
 
 # Using User Input
-a = input("First Number: ")
+""" a = input("First Number: ")
 b = input("Second Number: ")
 
 res = int(a) + int(b)
 
 print(res)
-
+ """
 # Using Functiions
+""" def add(num1,num2):
+    return num1 + num2
+
+a = 10
+b = 20
+
+print(add(a,b)) """
+
+# Lambda Functions
+""" res = lambda a,b : a + b
+print(res(10,5)) """
+
+# Using Operator module
+""" import operator
+print(operator.add(10,5)) """
+
+# Using sum()
+print(sum([10,5]))
